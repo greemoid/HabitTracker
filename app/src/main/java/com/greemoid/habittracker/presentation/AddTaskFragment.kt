b292be6abae508a66dc90e871d61f8361063fe6e
greemoid <greemoiddd@gmail.com>
2022-10-05 18:37:32 +0300

    added checkboxes, icons, radiobuttons and other

--- app/src/main/java/com/greemoid/habittracker/presentation/AddTaskFragment.kt
+++ app/src/main/java/com/greemoid/habittracker/presentation/AddTaskFragment.kt
@@ -1,7 +1,9 @@
 package com.greemoid.habittracker.presentation
 
-import android.util.Log
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import com.greemoid.habittracker.R
 import com.greemoid.habittracker.databinding.FragmentAddTaskBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,8 +13,25 @@ class AddTaskFragment :
     override val viewModel: EmptyViewModel by viewModels()
 
     override fun init() {
-        binding.rbDay.setOnCheckedChangeListener { _, b ->
-            Log.d("RADIO", b.toString())
+        binding.btnExit.navigate(R.id.action_addTaskFragment_to_tasksListFragment)
+        //todo if i open icons and without closing icons open colors i dont see anything
+        binding.btnChooseColor.setOnClickListener {
+            binding.linearWithCards.visibility =
+                if (binding.linearWithCards.isVisible) View.GONE else View.VISIBLE
+            binding.cardViewWithColors.visibility =
+                if (binding.cardViewWithColors.isVisible) View.GONE else View.VISIBLE
+            binding.cardViewWithIcons.visibility = View.GONE
+        }
+        binding.btnChooseIcon.setOnClickListener {
+            binding.linearWithCards.visibility =
+                if (binding.linearWithCards.isVisible) View.GONE else View.VISIBLE
+            binding.cardViewWithIcons.visibility =
+                if (binding.cardViewWithIcons.isVisible) View.GONE else View.VISIBLE
+            binding.cardViewWithColors.visibility = View.GONE
+        }
+        binding.btnOpenRepeats.setOnClickListener {
+            binding.linearWithCheckboxes.visibility =
+                if (binding.linearWithCheckboxes.isVisible) View.GONE else View.VISIBLE
         }
     }
 
